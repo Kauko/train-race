c9fa92b3b5ff174ddc8ca436c5cf209fdf05a8c5
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-03-02 20:21:13 +0200

    Remove faulty :on-jsload

--- env/dev/cljs/train_race/app.cljs
+++ env/dev/cljs/train_race/app.cljs
@@ -6,7 +6,6 @@
 (enable-console-print!)
 
 (figwheel/watch-and-reload
-  :websocket-url "ws://localhost:3449/figwheel-ws"
-  :on-jsload core/mount-components)
+  :websocket-url "ws://localhost:3449/figwheel-ws")
 
 (devtools/install!)
